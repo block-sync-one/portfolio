f90a117decdf9b037451e8453483a16bda30dc22
Collins Kamgaing <collinskamgaing@gmail.com>
2025-04-17 17:09:51 +0200

    add corresponding networkId to labels

--- packages/plugins/src/plugins/switchboard/onDemandPricingJob.ts
+++ packages/plugins/src/plugins/switchboard/onDemandPricingJob.ts
@@ -74,6 +74,6 @@ const executor: JobExecutor = async (cache: Cache) => {
 const job: Job = {
   id: `${platformId}-ondemand-pricing`,
   executor,
-  labels: ['realtime'],
+  labels: ['realtime', NetworkId.solana],
 };
 export default job;
